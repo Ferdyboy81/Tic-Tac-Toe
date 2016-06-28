68ed2852c1a7d2ecd6ef23f8866164a8ecb6efa9
Fernand Tan <ferdyboy81@gmail.com>
2016-06-28 11:20:03 -0700

    clicking on squares turns complete

--- js/script.js
+++ js/script.js
@@ -1,9 +1,40 @@
-var board = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'];
+
+//Models
+var board = ['X', '', '', '', '', '', '', '', ''];
 var turn = 'X'
 
+//selectors
+var squares = document.querySelectorAll('.square')
+
+
+//set up event listener or click listeners for my squares
+  for (var i=0; i<9; i++) {
+    squares[i].addEventListener('click', function (e) {
+      // loop through squares again and check which node was clicked
+      for (var j=0; j < 9; j++) {
+        if (squares[j] === e.target) {
+            board[j] = turn
+            drawboard()
+            switchTurn()
+        }
+      }
+    })
+  }
+
+
+//Drawing model to screen
 function drawboard() {
-  var squares = document.querySelectorAll('.square')
   for (var i=0; i<9; i++) {
     squares[i].innerHTML = board[i]
   }
 }
+
+// switching turns
+function switchTurn() {
+  if (turn === 'X')
+    turn = 'O'
+  else
+    turn = 'X'
+}
+
+
